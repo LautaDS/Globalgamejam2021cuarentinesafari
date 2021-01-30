af1d99ada6cf72ff7c8b158a7ee54dd098f0d60e
Lautaro De Simone <sistemas.desimone@gmail.com>
2021-01-30 12:02:23 -0300

    First push saturday
    Starting to get some elbow grease.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public GameObject flashlight;
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float drag;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
     void Start()
     {
         
@@ -15,4 +27,46 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    #region InputReciever
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        // Store data from inputs  
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnAction(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+           
+
+        }
+    }
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+
+
+        }
+    }
+
+    public void OnStart(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+
+
+        }
+    }
+
+    #endregion
+
+    private void Move()
+    {
+        rb.AddForce(new Vector2(movement.x * speed, movement.y * speed), ForceMode2D.Force);
+    }
+        
 }
